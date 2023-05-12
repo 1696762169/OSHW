79bf091fecd19d1239ea17f57790f220af3d0a8e
SZMSZ <75566373+1696762169@users.noreply.github.com>
2023-05-13 00:54:35 +0800

    定义了实现格式化所需的类

--- OSHW/include/DiskManager.h
+++ OSHW/include/DiskManager.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 #include "Inode.h"
 
 /**
@@ -19,4 +20,11 @@ public:
 	static constexpr int INODE_PER_SECTOR = SECTOR_SIZE / sizeof(DiskInode);	// 每个磁盘块可以存放的外存Inode数
 	static constexpr int INODE_START_SECTOR = SUPER_BLOCK_SECTOR + 2;	// 外存Inode区位于磁盘上的起始扇区号
 	static constexpr int INODE_SIZE = DATA_START_SECTOR - INODE_START_SECTOR;	// 外存Inode区占用的盘块数
+
+	static std::string imgPath;	// img文件路径
+
+public:
+	static void Init();	// 格式化img文件
+	static void Read(const char* content, int size, int offset);	// 从img文件读取内容到内存
+	static void Write(const char* content, int size, int offset);	// 将内存中的内容写入img文件
 };
--- OSHW/include/SuperBlock.h
+++ OSHW/include/SuperBlock.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <memory>
+#include "Buffer.h"
+#include "DiskManager.h"
 
 /**
  * \brief 超级块 定义文件系统的全局变量
@@ -6,5 +9,30 @@
 class SuperBlock
 {
 public:
+	static constexpr int FREE_SECTOR_CAPACITY = 120;	// 直接管理的空闲盘块最大数量
+	static constexpr int INODE_CAPACITY = 120;	// 直接管理的空闲外存Inode最大数量
+	static constexpr int PADDING_SIZE = 2 * DiskManager::SECTOR_SIZE / 4 - FREE_SECTOR_CAPACITY - INODE_CAPACITY - 2 - 2;	// 填充区大小
 
+public:
+	static std::unique_ptr<SuperBlock> Init();	// 初始化超级块
+	static std::unique_ptr<SuperBlock> Load();	// 从磁盘加载超级块
+	void Update();	// 更新超级块到磁盘
+
+	std::shared_ptr<Inode> AllocInode();	// 分配一个Inode
+	void FreeInode(int number);	// 释放一个Inode
+
+	std::shared_ptr<Buffer> AllocBuffer();	// 分配一个缓冲区
+	void FreeBuffer(int number);	// 释放一个缓冲区
+
+private:
+	int freeCount;	// 直接管理的空闲盘块数
+	int freeSectors[FREE_SECTOR_CAPACITY];	// 直接管理的空闲盘块索引表
+
+	int inodeCount;	// 直接管理的空闲外存Inode数
+	int inodes[INODE_CAPACITY];	// 直接管理的空闲外存Inode索引表
+
+	int modified;	// 被更改标识
+	int modifyTime;	// 最近一次更新时间
+
+	int padding[PADDING_SIZE];	// 填充区
 };
